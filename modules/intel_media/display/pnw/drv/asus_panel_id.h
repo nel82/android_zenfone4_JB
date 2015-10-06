8e518ae77a3ee7845771d96ff1aa8738c771ae4f
Nelsen G <nelsen.matra82@gmail.com>
2015-10-06 19:05:09 +0700

    modules/intel_media

---
+++ modules/intel_media/display/pnw/drv/asus_panel_id.h
@@ -0,0 +1,20 @@
+#ifndef __ASUS_PANEL_ID_H__
+#define __ASUS_PANEL_ID_H__
+
+#define ME302C_PANEL 0x10
+//CPT:0x10
+//AUO:0x11
+
+
+#define ME372CG_PANEL 0x20
+//INNOLUX(NT35521):0x20
+
+
+#define TX201LA_PANEL 0x30
+//BOE:0x30
+//Innolux:0x31
+
+
+#define TX301L_PANEL 0x40
+//Innolux:0x40
+#endif
